e98ff623c02cf0e154162c517c4b9901a9d6a5b9
th1nksnow <buffallo654@yandex.ru>
2025-07-24 00:09:46 +0300

    init

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -1,9 +1,14 @@
 package personaldata
 
+import "fmt"
+
 type Personal struct {
-	// TODO: добавить поля
+	Name           string
+	Weight, Height float64
 }
 
 func (p Personal) Print() {
-	// TODO: реализовать функцию
+
+	outputFormat := "Имя: %d\nВес: %.2f\nРост: %.2f\n"
+	fmt.Printf(outputFormat, p.Name, p.Weight, p.Height)
 }
--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,13 +1,75 @@
 package trainings
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type Training struct {
-	// TODO: добавить поля
+	personaldata.Personal
+	Steps        int
+	TrainingType string
+	Duration     time.Duration
 }
 
 func (t *Training) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+
+	parsedData := strings.Split(datastring, ",")
+	if len(parsedData) != 3 {
+		return errors.New("incorrect input format")
+	}
+
+	t.Steps, err = strconv.Atoi(parsedData[0])
+	if err != nil {
+		return err
+	}
+	if t.Steps <= 0 {
+		return errors.New("number of steps must be greater than 0")
+	}
+
+	t.Duration, err = time.ParseDuration(parsedData[2])
+	if err != nil {
+		return err
+	}
+	if t.Duration <= 0 {
+		return errors.New("training duration must be greater than 0")
+	}
+
+	return err
 }
 
 func (t Training) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+
+	var spentCalories float64
+	var err error
+
+	distance := spentenergy.Distance(t.Steps, t.Height)
+
+	meanSpeed := spentenergy.MeanSpeed(t.Steps, t.Height, t.Duration)
+
+	switch t.TrainingType {
+	case "Бег":
+		spentCalories, err = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+		if err != nil {
+			return "", err
+		}
+	case "Ходьба":
+		spentCalories, err = spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+		if err != nil {
+			return "", err
+		}
+	default:
+		return "", errors.New("unknown training type")
+	}
+
+	outputFormat := "Тип тренировки: %s\nДлительность: %0.2f ч.\nДистанция: %0.2f км.\nСкорость: %0.2f км/ч\nСожгли калорий: %0.2f\n"
+	output := fmt.Sprintf(outputFormat, t.TrainingType, t.Duration, distance, meanSpeed, spentCalories)
+
+	return output, nil
 }
